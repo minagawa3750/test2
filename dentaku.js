d729f2b7181c1717c5a70105a084197b16d3cc89
EC2 Default User <ec2-user@ip-172-31-43-251.us-west-2.compute.internal>
2022-06-29 09:24:28 +0000

    クラスをｊｓ変数resultに代入しているがconsole.logで取得できない

---
+++ dentaku.js
@@ -0,0 +1,10 @@
+function clickbutton(target) {
+  let result = document.getElementsByClassName("result");
+  console.log(result.innerHTML);
+  let target_value = result.innerHTML;
+  
+  if(target_value == "AC") {
+    console.log("AC");
+    
+   }
+  }
